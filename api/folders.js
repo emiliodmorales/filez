be291a31bed7ce7d70c2c984ce3b37040e051df2
Froggles <elmsterm@gmail.com>
2026-02-23 11:20:45 -0500

    add folder id param route

--- api/folders.js
+++ api/folders.js
@@ -2,9 +2,17 @@ import { Router } from "express";
 const router = Router();
 export default router;
 
-import { getFolders } from "#db/queries/folders";
+import { getFolder, getFolders } from "#db/queries/folders";
 
 router.get("/", async (req, res) => {
   const folders = await getFolders();
   res.send(folders);
+});
+
+router.param("id", async (req, res, next, id) => {
+  const folder = await getFolder(id);
+  if (!folder) return res.status(404).send("Folder not found.");
+
+  req.folder = folder;
+  next();
 });
