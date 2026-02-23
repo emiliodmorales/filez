f606f1215e9347c94f67ee166c74af2f240c3223
Froggles <elmsterm@gmail.com>
2026-02-23 11:05:04 -0500

    write GET /files route

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+import { getFiles } from "#db/queries/files";
+import { getFolder } from "#db/queries/folders";
 import express from "express";
 const app = express();
 export default app;
+
+app.get("/files", async (req, res) => {
+  const files = await getFiles();
+  for (const file of files) {
+    const folder = await getFolder({ id: file.folder_id });
+    file.folder_name = folder.name;
+  }
+  res.send(files);
+});
