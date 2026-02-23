40edff80d26c4644ff9a98f83a5ee0e7f1e38284
Froggles <elmsterm@gmail.com>
2026-02-23 10:56:33 -0500

    write seed function

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,6 @@
 import db from "#db/client";
+import { createFile } from "#db/queries/files";
+import { createFolder } from "#db/queries/folders";
 
 await db.connect();
 await seed();
@@ -6,5 +8,18 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  // TODO
+  const FOLDER_NAMES = ["one", "two", "three"];
+  const FILE_NAMES = ["one", "two", "three", "four", "five"];
+
+  for (const folderName of FOLDER_NAMES) {
+    const { id: folderId } = await createFolder({ name: folderName });
+
+    for (const fileName of FILE_NAMES) {
+      await createFile({
+        name: fileName,
+        folder_id: +folderId,
+        size: Math.floor(Math.random() * 10000),
+      });
+    }
+  }
 }
