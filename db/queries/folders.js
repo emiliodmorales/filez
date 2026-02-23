cf67324b8f0369ae98853fd905b7dbfa035ff039
Froggles <elmsterm@gmail.com>
2026-02-23 10:56:20 -0500

    write create folder query

---
+++ db/queries/folders.js
@@ -0,0 +1,17 @@
+import db from "#db/client";
+
+export async function createFolder({ name }) {
+  const SQL = `
+    INSERT INTO folders (name)
+    VALUES ($1)
+    RETURNING *
+  `;
+  try {
+    const {
+      rows: [folder],
+    } = await db.query(SQL, [name]);
+    return folder;
+  } catch (err) {
+    console.error(err);
+  }
+}
