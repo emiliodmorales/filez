e949f1c8fbb2cd118b8b2e8b4cd9dbf243d97bcc
Froggles <elmsterm@gmail.com>
2026-02-23 11:16:02 -0500

    route /files and /folders

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-import { getFiles } from "#db/queries/files";
-import { getFolder } from "#db/queries/folders";
 import express from "express";
 const app = express();
 export default app;
 
-app.get("/files", async (req, res) => {
-  const files = await getFiles();
-  for (const file of files) {
-    const folder = await getFolder({ id: file.folder_id });
-    file.folder_name = folder.name;
-  }
-  res.send(files);
-});
+import fileRouter from "#api/files";
+import folderRouter from "#api/folders";
+
+app.use("/files", fileRouter);
+
+app.use("/folders", folderRouter);
