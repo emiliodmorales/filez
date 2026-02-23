2a155ce9ea56b0024d9586bbc32183b753f8f185
Froggles <elmsterm@gmail.com>
2026-02-23 11:31:37 -0500

    fix function call param structuring

--- api/files.js
+++ api/files.js
@@ -8,7 +8,7 @@ import { getFolder } from "#db/queries/folders";
 router.get("/", async (req, res) => {
   const files = await getFiles();
   for (const file of files) {
-    const folder = await getFolder({ id: file.folder_id });
+    const folder = await getFolder(file.folder_id);
     file.folder_name = folder.name;
   }
   res.send(files);
