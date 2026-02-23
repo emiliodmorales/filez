60afc63aff9a3aa00b1dfdaf65907bf54c249ef7
Froggles <elmsterm@gmail.com>
2026-02-23 10:56:08 -0500

    write database schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,16 @@
--- TODO
+DROP TABLE IF EXISTS folders CASCADE;
+
+CREATE TABLE
+  folders (id SERIAL PRIMARY KEY, name TEXT UNIQUE NOT NULL);
+
+DROP TABLE IF EXISTS files;
+
+CREATE TABLE
+  files (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    size INT NOT NULL,
+    folder_id INT NOT NULL,
+    UNIQUE (folder_id, name),
+    FOREIGN KEY (folder_id) REFERENCES folders ON DELETE CASCADE
+  );
